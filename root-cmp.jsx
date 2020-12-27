f6163c3d334075bce29ca774b66c46e3693f4982
Eyal Alfasi <eyalalf@gmail.com>
2020-12-27 02:28:07 +0200

    final fix for header (hopefully)

--- root-cmp.jsx
+++ root-cmp.jsx
@@ -1,4 +1,4 @@
-import { _AppHeader } from './cmps/Header.jsx';
+import { AppHeader } from './cmps/Header.jsx';
 import { About } from './pages/about.jsx'
 import { BookApp } from './pages/book-app.jsx'
 import { Home } from './pages/home.jsx'
@@ -9,7 +9,7 @@ const { Route, Switch } = ReactRouterDOM;
 export function App() {
     return <Router>
         < section className="app" >
-            <_AppHeader />
+            <AppHeader />
             <Switch>
                 <Route path="/bookApp/:bookId" component={BookDetails} />
                 <Route path="/about" component={About} />
